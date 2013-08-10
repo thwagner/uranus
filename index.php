764e29764a7dbae7ee396e0f0fbb47c86bb9bfaf
mizech <michael.zech@googlemail.com>
2013-08-10 16:05:46 +0200

    10_08_2013_16_05_slide_show

--- index.php
+++ index.php
@@ -14,39 +14,25 @@
 
                     echo '<ul id="headlines"></ul>';
 
-                    echo '<h2>Autos</h2>
+                    echo '<h2 id="bilder">Bilder</h2>
                         <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, 
                         arcu.<br />In enim justo, rhoncus ut, imperdiet a, venenatis vitae, 
-                        justo. Nullam dictum felis eu pede mollis pretium.</p>
-                        <p>Proin pretium, leo ac pellentesque mollis, felis nunc ultrices 
-                        eros, sed gravida augue augue mollis justo. Suspendisse eu ligula. 
-                        Nulla facilisi. Donec id justo. Praesent porttitor, nulla vitae 
-                        posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut 
-                        ipsum. Curabitur suscipit suscipit tellus. </p>
-                        ';
+                        justo. Nullam dictum felis eu pede mollis pretium.</p>';
+                    
+                    echo '<ul class="slideshow">';
+                        echo '<li><img src="./images/beach.jpg" alt="bild" /></li>';
+                        echo '<li><img src="./images/bird.jpg" alt="bild" /></li>';
+                        echo '<li><img src="./images/garden.jpg" alt="bild" /></li>';
+                        echo '<li><img src="./images/sea.jpg" alt="bild" /></li>';                       
+                    echo '</ul>';
 
-                    echo '<h2>LKW</h2>
+                    echo '<h2>Beschreibung</h2>
                         <p>Phasellus viverra nulla ut metus varius laoreet. Quisque 
                         rutrum. Aenean imperdiet. Etiam ultricies nisi vel <br /> augue. 
                         Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam 
                         rhoncus.</p>'; 
 
-                    echo '<h2>Motorräder</h2>                       
-                        <p>Nam quam nunc, blandit vel, 
-                        luctus pulvinar, hendrerit id, lorem. 
-                        Maecenas nec odio et ante tincidunt tempus.<br />Donec vitae sapien ut libero 
-                        venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros 
-                        faucibus tincidunt.<br />Duis leo. Sed fringilla mauris sit amet nibh.</p>
-                        <p>In hac habitasse platea dictumst. Curabitur at lacus ac velit ornare 
-                        lobortis. Curabitur a felis in nunc fringilla tristique. Morbi mattis 
-                        ullamcorper velit. Phasellus gravida semper nisi. Nullam vel sem. 
-                        Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, 
-                        quam. Sed hendrerit. Morbi ac felis.
-                        </p>';
-
-                    echo '<h2>Sportwagen</h2>
-                        <p>Mauris turpis nunc, blandit et, volutpat molestie, porta ut, ligula. 
-                        Fusce pharetra convallis urna. Quisque ut nisi. </p>';
+                    
                                    
                 echo '</div>';
         ?>
--- js/jfunctions.js
+++ js/jfunctions.js
@@ -1,13 +1,37 @@
 $(function() {
+    $('#headlines').prepend('<ul id="pic_ul"></ul>');
     
-    $('#top_headline').prepend('<ul id="headlines"');
-    
-    $('h2').each(function(i) {
+    $('#wrapper h2').each(function(i) {
         $(this).attr('id', 'headline_' + i);
-        $('#headlines').append('<li><a href="index.php#' + 
-                'headline_' + i + '">' + 
-                $(this).html() + '</a></li>');
+        $('#pic_ul').append('<li><a href="#' 
+                + 'headline_' + i + '">' 
+                + $(this).html() + '</a></li>');
     });
     
-    $('#top_headline').append('</ul');
-})
+    $('.slideshow li').hide();
+    $('.slideshow li').eq(0).show().addClass('active');
+    $('.slideshow').wrap('<div class="slideshowcontainer"></div>').
+            parent().after(
+                '<a href="#" id="back">Back</a>\n\
+                    &nbsp;<a href="#" id="forward">Forward</a>'
+                ); 
+                    
+    $('#forward').click(function() {
+        $('li.active').removeClass('active').fadeOut(2000).next().addClass('active').fadeIn(2000);
+        // return false ist eine Anweisung an den Browser, das Ansteuern eines
+        // Hyperlink abzubrechen.
+        return false;
+    })
+    
+    $('#back').click(function() {
+        
+        if ($('li.active').next().length == 0) {
+            $('li.active').removeClass('active').fadeOut(2000).
+                    parent().find('li').eq(0).addClass('active');
+        } else {
+            $('li.active').removeClass('active').fadeOut(2000).prev().addClass('active').fadeIn(2000);       
+        }
+        
+        return false;       
+    })
+}) 
